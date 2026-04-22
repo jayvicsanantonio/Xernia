09ff8efc2fa0c9938875d5eda813a3dca7bb0781
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2026-04-22 00:29:12 -0700

    Initial Commit

---
+++ Xernia/XerniaApp.swift
@@ -0,0 +1,17 @@
+//
+//  XerniaApp.swift
+//  Xernia
+//
+//  Created by Jayvic San Antonio on 4/22/26.
+//
+
+import SwiftUI
+
+@main
+struct XerniaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ XerniaTests/XerniaTests.swift
@@ -0,0 +1,18 @@
+//
+//  XerniaTests.swift
+//  XerniaTests
+//
+//  Created by Jayvic San Antonio on 4/22/26.
+//
+
+import Testing
+
+struct XerniaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
